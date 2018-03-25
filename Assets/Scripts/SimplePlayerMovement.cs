87df77df57cfe26325640a92b595383ff5a926af
Marcellus Parley <msp261@humboldt.edu>
2018-03-24 21:39:48 -0700

    Basic player movement script added
    Added a test scene and basic player movement script. Tested and worked on my android and PC.  Only jumping works on Android for now because the lack of touch control buttons but left right movement is available on PC with the arrow keys. I don't know if this is the jumping control scheme we want; when I was thinking about it I figured this would method would be good for touch controls but if we decide to change it, it should be easy to change. If we do go with this drag and release style it would have to be changed to only start when a certain area of the screen is touched - I'm guessing around the player - as we will need the rest of the screen space for other ui/control elements.

---
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlayerMovement : MonoBehaviour {
+
+    public int speed = 10;
+    public float xAxis;
+    public float jumpFactor = 20.0f;
+    private bool jumpReady = false;
+    public Vector2 startTouch;
+    public Vector2 endTouch;
+    public bool grounded;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        Rigidbody2D physicsBody = gameObject.GetComponent<Rigidbody2D>();
+
+        // This gets the horizontal axis and applies velocity to the player's physics
+        xAxis = Input.GetAxis("Horizontal");
+        if (!jumpReady)
+            physicsBody.velocity = new Vector2(xAxis * speed, physicsBody.velocity.y);
+
+        // This block controlls jumping. As it is here the user presses LMB (which should
+        // also register as a touch) and then drags to a position in the opposite direction
+        // he/she wants to leap - ie dragging down-left will leap up and to the right. On
+        // release of the button/touch we grab the point and then use the angle and distance
+        // between the points to add a force making the player jump.
+        if (Input.GetMouseButtonDown(0) && grounded)
+        {
+            jumpReady = true;
+            startTouch = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else if (Input.GetMouseButtonUp(0) && grounded)
+        {
+            endTouch = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            float thrust = Vector2.Distance(endTouch, startTouch) * jumpFactor;
+            physicsBody.AddForce((startTouch-endTouch).normalized * thrust);
+            grounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+}
