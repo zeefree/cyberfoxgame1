abaf82018289a0d7e540452d0257e3e7cd3f4571
Lelouch87 <chasecullen@gmail.com>
2018-04-01 21:00:03 -0700

    Camera & Sprite
    created a fully functioning sprite to test animations and added 2 camera scripts

---
+++ Assets/Scripts/GroundedCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedCheck : MonoBehaviour {
+	private Player player;
+
+
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<Player>();	
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		player.grounded = true;
+	}	
+
+	void OnTriggerExit2D(Collider2D col)
+	{
+		player.grounded = false;
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		player.grounded = true;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed = 3;
+	public float speed = 50f;
+	public float jumpPower = 150f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+		if (Input.GetAxis ("Horizontal") < -0.1f) {
+			transform.localScale = new Vector3 (-2, 2, 1);
+		}
+
+		if (Input.GetAxis ("Horizontal") > 0.1f) {
+			transform.localScale = new Vector3 (2, 2, 1);
+		}
+	}
+		
+}
