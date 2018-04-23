4ced12035675abe5e1ebdc00b37d2b909b858266
Zach Freeman <zfreeman997@gmail.com>
2018-04-23 14:47:53 -0700

    swipedetection added, don't worry about it

---
+++ Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour {
+
+
+    private float first_point;
+    private float last_point;
+    private float drag_distance;
+	// Use this for initialization
+	void Start ()
+    {
+		drag_distance = 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    	
+	}
+}
